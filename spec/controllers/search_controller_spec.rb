7fef3a6e77e74dd4f2eaf0b7c5069160236be1cd
Eddie Glenn <glen0071@gmail.com>
2019-07-22 21:46:54 -0500

    start search specs

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET #new" do
     it "returns http success" do
-      get :index
+      get :new
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #create" do
+    it "returns http success" do
+      post :create, params: {query: { q: 'Truth' } }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns matching topics and quotes" do
+      
     end
   end
 
---
+++ spec/factories/quotes_factory.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :theme do
+    text { "Truthfulness" }
+    source { "Source" }
+    source_link { "source_link.com" }
+    year { "2000 AD" }
+  end
+end
