0b9a02bc442ac8a6f7f76a03c58d577721c56dcd
Eddie <glen0071@gmail.com>
2019-08-31 19:26:03 -0500

    react search bar

--- app/javascript/packs/search_bar.jsx
+++ app/javascript/packs/search_bar.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const inputChange = (e) => {
+  var value = e.target.value
+}
+
 const SearchBar = () => (
-  <h1>
-  </h1>
+  <form
+    className='form-inline main-search-form'
+    >
+    <input
+      className='form-control mr-sm-2 main-search-bar'
+      placeholder='Search by Text or Topic'
+      id='main-search-input'
+      autoComplete='off'
+      onChange={inputChange}
+      >
+    </input>
+    <input
+      type='submit'
+      className='btn btn-outline-success my-2 my-sm-0' >
+    </input>
+  </form>
+
 )
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <SearchBar />,
-    document.body.appendChild(document.createElement('div')),
+    document.getElementById('search-bar-mount'),
   )
 })
