784dbae95ac89e3f83a3d9925c382db95c23ed36
Eddie <glen0071@gmail.com>
2020-03-06 14:01:25 -0600

    give ./Word reference a suffix

--- app/javascript/packs/study/CompleteQuote.jsx
+++ app/javascript/packs/study/CompleteQuote.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-import Word from './Word'
+import Word from './Word.jsx'
 import PropTypes from 'prop-types'
 
 const CompleteQuote = props => {
