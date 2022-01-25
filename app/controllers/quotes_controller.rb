f9924ae321ea8797a0f31a11bd02e0b18d1f033d
eddie_g <glen0071@gmail.com>
2022-01-25 01:45:12 -0600

    better quote form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -22,6 +22,21 @@ class QuotesController < ApplicationController
   def create
     @quote = Quote.new(quote_params)
 
+    new_author = quote_params.delete(:new_author)
+    if new_author.present?
+      @quote.author = Author.find_or_create_by(name: new_author)
+    end
+    
+    new_source = quote_params.delete(:new_source)
+    if new_source.present?
+      @quote.source = Source.find_or_create_by(title: new_source)
+    end
+
+    new_topics = quote_params.delete(:new_topics)&.split(',').uniq
+    new_topics.each do |topic|
+      @quote.topics << Topic.find_or_create_by(name: topic)
+    end
+
     respond_to do |format|
       if @quote.save
         format.html { redirect_to @quote, notice: 'Quote was successfully created.' }
@@ -63,8 +78,10 @@ class QuotesController < ApplicationController
   end
 
   def quote_params
-    params.require(:quote).permit(:text, :source_title, :source_link, :author_id,
-                                  :source_id, :good, :context, :date, :page,
-                                  :locked, topic_ids: [])
+    params
+    .require(:quote)
+    .permit(:text, :source_title, :source_link, :author_id,
+      :source_id, :good, :context, :date, :page, :new_author, :new_source,
+      :locked, :new_topics, topic_ids: [])
   end
 end
