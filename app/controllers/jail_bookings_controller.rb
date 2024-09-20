3f84ba9307cec998ec1ba6ab78b5e2121f13197a
Eddie Glenn <hello@eddieg.me>
2024-09-19 23:56:20 -0500

    configure ferrum in heroku

--- app/controllers/jail_bookings_controller.rb
+++ app/controllers/jail_bookings_controller.rb
@@ -1,5 +1,5 @@
 class JailBookingsController < ApplicationController
-  before_action :set_jail_booking, only: %i[ show edit update destroy ]
+  before_action :set_jail_booking, only: %i[show edit update destroy]
 
   # GET /jail_bookings
   def index
@@ -7,8 +7,7 @@ class JailBookingsController < ApplicationController
   end
 
   # GET /jail_bookings/1
-  def show
-  end
+  def show; end
 
   # GET /jail_bookings/new
   def new
@@ -16,15 +15,14 @@ class JailBookingsController < ApplicationController
   end
 
   # GET /jail_bookings/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /jail_bookings
   def create
     @jail_booking = JailBooking.new(jail_booking_params)
 
     if @jail_booking.save
-      redirect_to @jail_booking, notice: "Jail booking was successfully created."
+      redirect_to @jail_booking, notice: 'Jail booking was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -33,7 +31,7 @@ class JailBookingsController < ApplicationController
   # PATCH/PUT /jail_bookings/1
   def update
     if @jail_booking.update(jail_booking_params)
-      redirect_to @jail_booking, notice: "Jail booking was successfully updated."
+      redirect_to @jail_booking, notice: 'Jail booking was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,17 +40,19 @@ class JailBookingsController < ApplicationController
   # DELETE /jail_bookings/1
   def destroy
     @jail_booking.destroy
-    redirect_to jail_bookings_url, notice: "Jail booking was successfully destroyed."
+    redirect_to jail_bookings_url, notice: 'Jail booking was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_jail_booking
-      @jail_booking = JailBooking.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def jail_booking_params
-      params.require(:jail_booking).permit(:number, :full_name, :age, :inmate_number, :custody_status, :housing_location, :received_date_time, :arrested_by, :released_date_tiem, :city, :state)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_jail_booking
+    @jail_booking = JailBooking.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def jail_booking_params
+    params.require(:jail_booking).permit(:number, :full_name, :age_at_booking, :inmate_number, :custody_status,
+                                         :housing_location, :received_date_time, :arrested_by, :released_date_time, :city, :state)
+  end
 end
--- spec/factories/jail_bookings.rb
+++ spec/factories/jail_bookings.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :jail_booking do
-    number { "MyString" }
-    full_name { "MyString" }
+    number { 'MyString' }
+    full_name { 'MyString' }
     age { 1 }
-    inmate_number { "MyString" }
-    custody_status { "MyString" }
-    housing_location { "MyString" }
-    received_date_time { "2024-09-08 06:53:31" }
-    arrested_by { "MyString" }
-    released_date_tiem { "2024-09-08 06:53:31" }
-    city { "MyString" }
-    state { "MyString" }
+    inmate_number { 'MyString' }
+    custody_status { 'MyString' }
+    housing_location { 'MyString' }
+    received_date_time { '2024-09-08 06:53:31' }
+    arrested_by { 'MyString' }
+    released_date_time { '2024-09-08 06:53:31' }
+    city { 'MyString' }
+    state { 'MyString' }
   end
 end
