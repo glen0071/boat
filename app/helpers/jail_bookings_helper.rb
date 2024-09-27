6d5a017970967ffb991dca60858e89f72b5884a2
Eddie Glenn <hello@eddieg.me>
2024-09-27 05:27:36 -0500

    useful index

--- app/helpers/jail_bookings_helper.rb
+++ app/helpers/jail_bookings_helper.rb
@@ -1,7 +1,1 @@
-module JailBookingsHelper
-  def how_long_in(jail_booking)
-    start_time = jail_booking.received_date_time&.to_datetime
-    end_time = jail_booking.released_date_time&.to_datetime || DateTime.now
-    (end_time - start_time).to_i
-  end
-end
+module JailBookingsHelper; end
