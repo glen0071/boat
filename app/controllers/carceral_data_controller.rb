b5c501209f4a3a47db4056a8f61a31ab4fda17df
Eddie Glenn <hello@eddieg.me>
2024-10-08 05:45:19 -0500

    display more data stats

--- app/controllers/carceral_data_controller.rb
+++ app/controllers/carceral_data_controller.rb
@@ -8,6 +8,10 @@ class CarceralDataController < ApplicationController
 
     @bail_amounts = HoldingCase.group(:bail_options).count.sort_by { |_key, value| -value }
 
+    @charges = CaseCharge.group(:description).count.sort_by { |_key, value| -value }
+
+    @charge_severity = CaseCharge.group(:severity_of_charge).count.sort_by { |_key, value| -value }
+
     @posted_bail_descriptions = HoldingCase.where(clear_reason: 'BAIL POSTED').map do |hc|
       hc.case_charges.first.description
     end
