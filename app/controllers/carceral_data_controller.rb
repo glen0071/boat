fbd0cab33c70274f676c5f295bb2373053359da5
Eddie Glenn <hello@eddieg.me>
2024-10-14 22:21:39 -0500

    a few things to make bail info more helpful

--- app/controllers/carceral_data_controller.rb
+++ app/controllers/carceral_data_controller.rb
@@ -4,11 +4,9 @@ class CarceralDataController < ApplicationController
     @jail_bookings_count = JailBooking.count
     @in_custody_now = JailBooking.where(custody_status: 'Currently in Jail/Custody').count
 
-    @arrested_by_rankings = JailBooking.group(:arrested_by).count.sort_by { |_key, value| -value }
+    @arrested_by_rankings = JailBooking.group(:arrested_by).count.sort_by { |_key, value| -value }.take(10)
 
-    @bail_amounts = HoldingCase.group(:bail_options).count.sort_by { |_key, value| -value }
-
-    @charges = CaseCharge.group(:description).count.sort_by { |_key, value| -value }
+    @charges = CaseCharge.group(:description).count.sort_by { |_key, value| -value }.take(10)
 
     @charge_severity = CaseCharge.group(:severity_of_charge).count.sort_by { |_key, value| -value }
 
@@ -17,5 +15,19 @@ class CarceralDataController < ApplicationController
     end
 
     @jailable_crimes
+  end
+
+  def hennepin_bail_amounts
+    @bail_amounts = HoldingCase.where.not(bail_options: nil).group(:bail_options).count.sort_by { |_key, value| -value }
+
+    @empty_bail_options = HoldingCase.where(bail_options: nil).count
+
+    all_bookings = JailBooking.includes(:holding_cases).all
+
+    nbr_amount = all_bookings.select { |jb| jb.bail_required_on_booking? == false }.count
+
+    not_nbr_amount = all_bookings.count - nbr_amount
+
+    @nbr_amount = [['No Bail Required', nbr_amount], ['Bail Required', not_nbr_amount]]
   end
 end
