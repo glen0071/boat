205e22bb42c320b7634fea6bd54097e4bee1d2f0
Eddie Glenn <hello@eddieg.me>
2024-10-14 19:17:53 -0500

    count the $$

--- app/models/holding_case.rb
+++ app/models/holding_case.rb
@@ -2,15 +2,68 @@ class HoldingCase < ApplicationRecord
   belongs_to :jail_booking
   has_many :case_charges, dependent: :destroy
 
-  # Bail Cateogirization
-  # - NBR
-  # - Must Pay
-  # - Must Pay: 78
-  # - Must Pay: $1,000+
-  # - Must Pay: $10,000+
-  # - Must Pay: $100,000+
-  #
+  before_save :set_bail_options
 
-  # More categories
-  # Before vs. after first appearance
+  scope :where_no_bail_required, lambda {
+    where('bail_options LIKE ? OR bail_options LIKE ? OR bail_options LIKE ? OR bail_options LIKE ?',
+          '%$0.00 NO BAIL REQUIRED%',
+          '%$0.00 RELEASE OWN RECOGNIZANCE%',
+          '%$0.00 BAIL/BOND W/O CR%',
+          '%$0.00 RELEASE WITH CONDITIONS%')
+  }
+
+  BOND_WITHOUT_CONDITIONS_REGEX = %r{(\$\d{0,3},?\d{0,3},?\d{1,3}\.00)\s?(BAIL/BOND W/O CR)}
+  CASH_WITHOUT_CONDITIONS_REGEX = %r{(\$\d{0,3},?\d{0,3},?\d{1,3}\.00)\s?(CASH BAIL W/O CR)}
+  BOND_WITH_CONDITIONS_REGEX = %r{(\$\d{0,3},?\d{0,3},?\d{1,3}\.00)\s?(BAIL/BOND W/ CR)}
+  CASH_WITH_CONDITIONS_REGEX = %r{(\$\d{0,3},?\d{0,3},?\d{1,3}\.00)\s?(CASH BAIL W/ CR)}
+  def set_bail_options
+    return nil if bail_options.nil?
+
+    self.bond_without_conditions = 0
+    self.cash_without_conditions = 0
+    self.bond_with_conditions = 0
+    self.cash_with_conditions = 0
+
+    if bail_options.match(BOND_WITHOUT_CONDITIONS_REGEX) && bail_options.match(BOND_WITHOUT_CONDITIONS_REGEX)[2].present?
+      self.bond_without_conditions = bail_options
+                                     .match(BOND_WITHOUT_CONDITIONS_REGEX)[1]
+                                     .gsub('$', '').gsub(',', '').to_i
+    end
+
+    if bail_options.match(CASH_WITHOUT_CONDITIONS_REGEX) && bail_options.match(CASH_WITHOUT_CONDITIONS_REGEX)[2].present?
+      self.cash_without_conditions = bail_options
+                                     .match(CASH_WITHOUT_CONDITIONS_REGEX)[1]
+                                     .gsub('$', '').gsub(',', '').to_i
+    end
+
+    if bail_options.match(BOND_WITH_CONDITIONS_REGEX) && bail_options.match(BOND_WITH_CONDITIONS_REGEX)[2].present?
+      self.bond_with_conditions = bail_options.match(BOND_WITH_CONDITIONS_REGEX)[1].gsub('$', '').gsub(',', '').to_i
+    end
+
+    if bail_options.match(CASH_WITH_CONDITIONS_REGEX) && bail_options.match(CASH_WITH_CONDITIONS_REGEX)[2].present?
+      self.cash_with_conditions = bail_options.match(CASH_WITH_CONDITIONS_REGEX)[1].gsub('$', '').gsub(',', '').to_i
+    end
+
+    self.no_bail_required = explicit_nbr? && implicit_nbr?
+  end
+
+  def explicit_nbr?
+    return nil if bail_options.nil?
+
+    # maybe sufficient to just check for $0.00
+    # erring on the side of precision here
+    # in case they ever explicate $0.00 for unconditional bail
+    bail_options.include?('$0.00 NO BAIL REQUIRED') ||
+      bail_options.include?('$0.00 RELEASE OWN RECOGNIZANCE') ||
+      bail_options.include?('$0.00 RELEASE WITH CONDITIONS') ||
+      bail_options.include?('$0.00 BAIL/BOND W/O CR')
+  end
+
+  def bail_amount_with_conditions
+    bond_with_conditions.to_i + cash_with_conditions.to_i
+  end
+
+  def implicit_nbr?
+    bail_amount_with_conditions.zero?
+  end
 end
--- app/models/jail_booking.rb
+++ app/models/jail_booking.rb
@@ -10,14 +10,21 @@ class JailBooking < ApplicationRecord
     (end_time - start_time).to_i
   end
 
-  # def money_holding
-  #   holding_cases.each do |holding_case|
-  #     # what to return for HWB?
-  #     next if holding_case.bail_options.nil?
+  def bail_required_on_booking
+    return nil if holding_cases.nil?
 
-  #     prices = holding_case.bail_options.scan(/\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?/)
-  #     prices.include?('$0.00')
-  #     prices.include?
-  #   end
-  # end
+    bail_decisions = holding_cases.map do |holding_case|
+      holding_case.set_bail_options if holding_case.no_bail_required.nil?
+
+      holding_case.no_bail_required
+    end
+
+    bail_decisions.include?(true)
+  end
+
+  def bail_amount
+    return 0 unless bail_required_on_booking
+
+    holding_cases.map(&:bail_amount_with_conditions).sum
+  end
 end
